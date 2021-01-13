456f5b120f3c3d878db15e4882aa36888a0cbdd2
moto007 <tomislavblaz@gmail.com>
2021-01-13 23:27:03 +0100

    first commit

--- vjesalo/Form1.Designer.cs
+++ vjesalo/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace vjesalo
+﻿namespace Vjesalo
 {
     partial class Form1
     {
@@ -28,12 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Location = new System.Drawing.Point(373, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 303);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(222, 284);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(355, 238);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Promasaji:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Duzina Rijeci:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 256);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(336, 59);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(87, 20);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(20, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Dodaj Rijec";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj Slovo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(613, 319);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Vjesalo";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- vjesalo/Form1.cs
+++ vjesalo/Form1.cs
@@ -4,11 +4,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace vjesalo
+
+namespace Vjesalo
 {
     public partial class Form1 : Form
     {
@@ -16,5 +18,168 @@ namespace vjesalo
         {
             InitializeComponent();
         }
+
+        string rec = "";
+
+        List<Label> labels = new List<Label>();
+
+        int kolicina = 0;
+
+        enum DjeloviTijela
+        {
+            Glava,
+            Lijevo_Oko,
+            Desno_Oko,
+            Usta,
+            Desna_Ruka,
+            Lijeva_Ruka,
+            Tijelo,
+            Desna_Noga,
+            Lijeva_Noga
+        }
+        void NacrtajVjesalo()
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Brown, 10);
+            g.DrawLine(p, new Point(210, 354), new Point(210, 5));
+            g.DrawLine(p, new Point(215, 5), new Point(85, 5));
+            g.DrawLine(p, new Point(80, 0), new Point(80, 50));       
+        }
+
+        void NacrtajDijeloveTijela(DjeloviTijela dt)
+        {
+            Graphics g = panel1.CreateGraphics();
+            Pen p = new Pen(Color.Blue, 2);
+            SolidBrush s = new SolidBrush(Color.DarkRed);
+            switch (dt) {
+                case DjeloviTijela.Glava:
+                    g.DrawEllipse(p, 52, 50, 52, 52);
+                    break;
+                case DjeloviTijela.Desno_Oko:
+                    g.FillEllipse(s, 83, 60, 5, 5);
+                    break;
+                case DjeloviTijela.Lijevo_Oko:
+                    g.FillEllipse(s, 63, 60, 5, 5);
+                    break;
+                case DjeloviTijela.Usta:
+                    g.DrawArc(p, 65, 65, 20, 20, 45, 90);
+                    break;
+                case DjeloviTijela.Tijelo:
+                    g.DrawLine(p, new Point(80, 100), new Point(80, 170));
+                    break;
+                case DjeloviTijela.Lijeva_Ruka:
+                    g.DrawLine(p, new Point(80, 120), new Point(30, 85));
+                    break;
+                case DjeloviTijela.Desna_Ruka:
+                    g.DrawLine(p, new Point(80, 120), new Point(130, 85));
+                    break;
+                case DjeloviTijela.Lijeva_Noga:
+                    g.DrawLine(p, new Point(80, 170), new Point(30, 250));
+                    break;
+                case DjeloviTijela.Desna_Noga:
+                    g.DrawLine(p, new Point(80, 170), new Point(130, 250));
+                    break;
+
+            }
+        }
+
+        void NapraviLabels()
+        {
+            rec = UzmiNasumicnuRijec();
+            char[] chars = rec.ToCharArray();
+            int izmedju = 330 / chars.Length - 1;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                labels.Add(new Label());
+                labels[i].Location = new Point((i * izmedju) + 10, 80);
+                labels[i].Text = "_";
+                labels[i].Parent = groupBox2;
+                labels[i].BringToFront();
+                labels[i].CreateControl();
+            }
+            label1.Text = "Duzina Rijeci: " + (chars.Length).ToString();
+        }
+
+        string UzmiNasumicnuRijec()
+        {
+
+            String[] text = System.IO.File.ReadAllLines(@"C:\Users\tomis\Desktop\projekti\Vjesalo\vjesalo\Rijeci.txt");
+            Random r = new Random();
+            return text[r.Next(0, text.Length-1)];
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            NacrtajVjesalo();
+            NapraviLabels();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char slovo = textBox1.Text.ToLower().ToCharArray()[0];
+            textBox1.Clear();
+            if (rec.Contains(slovo))
+            {
+                char[] slova = rec.ToCharArray();
+                for (int i = 0; i < slova.Length; i++)
+                {
+                    if (slova[i] == slovo)
+                        labels[i].Text = slovo.ToString();
+                }
+                foreach (Label l in labels)
+                    if (l.Text == "_") return;
+                MessageBox.Show("Pobjedili ste!", "Cestitamo");        
+                Reset();
+            }
+            else
+            {
+                MessageBox.Show("Slovo koje ste probali nije u rijeci!");
+                label2.Text += " " + slovo.ToString() + ",";
+                NacrtajDijeloveTijela((DjeloviTijela)kolicina);
+                kolicina++;
+                if (kolicina == 9)
+                {
+                    MessageBox.Show("Izgubili ste! Rijec je bila " + rec);
+                    Reset();
+                }
+            }
+            
+        }
+        void Reset()
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(panel1.BackColor);
+            UzmiNasumicnuRijec();
+            NapraviLabels();
+            NacrtajVjesalo();
+            label2.Text = "Promasaji: ";
+            textBox1.Text = "";
+            kolicina = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == rec)
+            {
+                MessageBox.Show("Pobjedili ste!", "Cestitamo");
+                textBox2.Clear();
+                Reset();
+            }
+            else
+            {
+                MessageBox.Show("Rijec koju ste probali je pogresna!", "Greska!");
+                NacrtajDijeloveTijela((DjeloviTijela)kolicina);
+                kolicina++;
+                if (kolicina == 9)
+                {
+                    MessageBox.Show("Izgubili ste! Rijec je bila " + rec);
+                    Reset();
+                }
+            }
+            textBox2.Clear();
+
+        }
+
+        
     }
 }
